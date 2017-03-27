46d92bae9d7ccf75aa0a837dc79b67a99815e94b
Flavio Kon Rosenfeld <flaviokr@gmail.com>
2017-03-26 21:48:49 -0300

    Refactor WhoSampled track importer

--- importers.rb
+++ importers.rb
@@ -1,38 +1,39 @@
 require 'mechanize'
 
 module Importers
-  class WhoSampled
+  module WhoSampled
+    class Tracks
+      def self.import(url)
+        agent = Mechanize.new
+        page = agent.get(url)
 
-    def self.track(url)
-      agent = Mechanize.new
-      page = agent.get(url)
+        track, artist, album = scrape_page(page)
 
-      track = Track.find_or_create_by(title: page.search('div.trackInfo h1[itemprop=name]').text,
-                                      release_date: page.search('div.trackInfo span[itemprop=datePublished]').text)
+        track.albums << album
+        album.artists << artist
 
-      artist = Artist.find_or_create_by(name: page.search('div.trackArtists h2 a').text)
+        page.search('.section + section a.playIcon').each do |sample_link|
+          compare_page = Mechanize::Page::Link.new(sample_link, agent, page).click
+          sample_page = compare_page.links_with(class: 'trackName').last.click
 
-      title = page.search('div.trackReleaseDetails h3.release-name a').text
-      album = Album.find_or_create_by(title: title.blank? ? "Single - #{artist.name}" : title)
+          sample, artist, album = scrape_page(sample_page)
 
-      track.albums << album
-      album.artists << artist
+          track.samples << sample
+          sample.albums << album
+          album.artists << artist
+        end
+      end
 
-      page.search('.section + section a.playIcon').each do |sample_link|
-        compare_page = Mechanize::Page::Link.new(sample_link, agent, page).click
-        sample_page = compare_page.links_with(class: 'trackName').last.click
+      def self.scrape_page(page)
+        track = Track.find_or_create_by(title: page.search('div.trackInfo h1[itemprop=name]').text,
+                                        release_date: page.search('div.trackInfo span[itemprop=datePublished]').text)
 
-        sample = Track.find_or_create_by(title: sample_page.search('div.trackInfo h1[itemprop=name]').text,
-                                         release_date: sample_page.search('div.trackInfo span[itemprop=datePublished]').text)
+        artist = Artist.find_or_create_by(name: page.search('div.trackArtists h2 a').text)
 
-        artist = Artist.find_or_create_by(name: sample_page.search('div.trackArtists h2 a').text)
-
-        title = sample_page.search('div.trackReleaseDetails h3.release-name a').text
+        title = page.search('div.trackReleaseDetails h3.release-name a').text
         album = Album.find_or_create_by(title: title.blank? ? "Single - #{artist.name}" : title)
 
-        track.samples << sample
-        sample.albums << album
-        album.artists << artist
+        [track, artist, album]
       end
     end
   end
